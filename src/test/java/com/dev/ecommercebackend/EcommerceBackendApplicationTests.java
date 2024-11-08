3c812376e2708ecf0a2c6028e60a50e69b41d8b5
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2024-11-08 13:40:44 +0200

    Initial commit

---
+++ src/test/java/com/dev/ecommercebackend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
